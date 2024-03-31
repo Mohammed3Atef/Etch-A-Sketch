2ee85ab568b6f6d78a93bba107e807a2a132804b
Mohammed3Atef <muhammedatef57@gmail.com>
2024-03-31 07:19:09 +0200

    update

--- main.js
+++ main.js
@@ -1,31 +1,31 @@
 // design squares
 let container = document.querySelector(".container");
 
-function getColors() {
-  const colors = [
-    "amber",
-    "lavender",
-    "navy",
-    "lime",
-    "burgundy",
-    "crimson",
-    "canary",
-    "hazel",
-    "sky blue",
-  ];
-  const randomcolor = Math.floor(Math.random() * 9);
-  const color = colors[randomcolor];
-  console.log(color);
+// function getColors() {
+//   const colors = [
+//     "amber",
+//     "lavender",
+//     "navy",
+//     "lime",
+//     "burgundy",
+//     "crimson",
+//     "canary",
+//     "hazel",
+//     "sky blue",
+//   ];
+//   const randomcolor = Math.floor(Math.random() * 9);
+//   const color = colors[randomcolor];
+//   console.log(color);
 
-  return color;
-}
+//   return color;
+// }
 
 for (let i = 0; i < 36; i++) {
   // let squares = `<div class="square"></div>`;
   let squares = document.createElement("div");
 
   squares.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = getColors();
+    e.target.style.backgroundColor = "blue";
   });
   // squares.addEventListener("mouseout", (e) => {
   //   e.target.style.backgroundColor = "black";
