b367ec21c17d78a6c8259bef77870e4669d7e467
Mohammed3Atef <muhammedatef57@gmail.com>
2024-04-02 06:30:07 +0200

    update data

--- main.js
+++ main.js
@@ -1,35 +1,31 @@
-// design squares
-let container = document.querySelector(".container");
-
-// function getColors() {
-//   const colors = [
-//     "amber",
-//     "lavender",
-//     "navy",
-//     "lime",
-//     "burgundy",
-//     "crimson",
-//     "canary",
-//     "hazel",
-//     "sky blue",
-//   ];
-//   const randomcolor = Math.floor(Math.random() * 9);
-//   const color = colors[randomcolor];
-//   console.log(color);
-
-//   return color;
-// }
-
-for (let i = 0; i < 36; i++) {
-  // let squares = `<div class="square"></div>`;
-  let squares = document.createElement("div");
-
-  squares.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "blue";
-  });
-  // squares.addEventListener("mouseout", (e) => {
-  //   e.target.style.backgroundColor = "black";
-  // });
-
-  container.appendChild(squares);
+//values
+let container = document.querySelector(".game");
+let submit = document.getElementById("submit");
+let displaySize = document.getElementById("size-label");
+let gridSize = document.getElementById("new-size");
+gridSize.value = 4;
+let newSize = gridSize.value;
+console.log(gridSize.value);
+//Range Bar
+displaySize.textContent = `${gridSize.value} * ${gridSize.value}`;
+gridSize.addEventListener("mousemove", function () {
+  displaySize.textContent = `${gridSize.value} * ${gridSize.value}`;
+});
+//get grid size
+function getGridSize(width, height) {
+  // let width = gridSize.value;
+  // let height = gridSize.value;
+  let dimensions = width * height;
+  for (let i = 0; i < dimensions; i++) {
+    let squares = document.createElement("div");
+    squares.style.width = `calc((100%) / ${width})`;
+    squares.style.height = `calc((100%) / ${height})`;
+    squares.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = "blue";
+    });
+    container.appendChild(squares);
+  }
 }
+submit.addEventListener("click", () => {
+  getGridSize(gridSize.value, gridSize.value);
+});
